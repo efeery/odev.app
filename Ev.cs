7f4174a6376818c40e4e8f547d5516f0320f7840
efeery <efeeryildirim109@gmail.com>
2024-06-10 15:39:47 +0300

    Add project files.

---
+++ Ev.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Odev
+{
+    public class Ev
+    {
+        public string Semt { get; set; }
+        public int KatNo { get; set; }
+        public int OdaSayisi { get; set; }
+        public double Alan { get; set; }
+
+        public override string ToString()
+        {
+            return $"Semt: {Semt}, Kat No: {KatNo}, Oda Sayısı: {OdaSayisi}, Alan: {Alan} m²";
+        }
+    }
+}
---
+++ SatilikEv.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Odev
+{
+    public class SatilikEv : Ev
+    {
+        public double SatisFiyati { get; set; }
+
+        public override string ToString()
+        {
+            return base.ToString() + $", Satış Fiyatı: {SatisFiyati:C}";
+        }
+    }
+}
